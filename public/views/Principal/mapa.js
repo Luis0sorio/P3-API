37dd9a5876c4c63a4737c1d7e67f9130010a3af3
Michael Giraldo <michaelgiraldo320@gmail.com>
2025-02-25 18:55:36 +0100

    nuevo front, y refactorizacion

--- public/views/Principal/mapa.js
+++ public/views/Principal/mapa.js
@@ -1,75 +1,209 @@
-// Localizar el body
+//Inicializacion del mapa y elementos del index.html
+let map;
 const body = document.querySelector("body");
-
-// Localizar el header
 const header = document.querySelector("header");
-
-// Localizar head
+const main = document.querySelector("main");
 const head = document.querySelector("head");
 
-// Añadir el nombre del usuario en el header
-const h2 = document.createElement("h2");
-h2.setAttribute("id", "titulo");
-// Temporal
-h2.textContent = "Hola Usuario";
+//funcion para inicializar la configuracion del mapa
 
-header.appendChild(h2);
-body.appendChild(header);
+function initMapa(){
+    agregarTituloUsuario();
+    agregarMapayBusqueda();
+    cargarMapbox();
+}
 
-// Localizar el main para agregar posteriormente el div que contendra el mapa
-const main = document.querySelector("main");
+//añadir titulo del usuario en el header
+function agregarTituloUsuario() {
+    const h2 = document.createElement("h2");
+    h2.setAttribute("id","titulo");
+    h2.textContent = "Hola Usuario";
+    header.appendChild(h2);
+    body.appendChild(header);
+}
 
-// Añadir divTodo
-const divTodo = document.createElement("div");
-divTodo.setAttribute("id", "divtodo");
-// Temporal
-divTodo.textContent = "Hola";
+//funcion para configurar el mapa y el buscador 
+function agregarMapayBusqueda() {
 
-// Añadir divMapa
-const divMapa = document.createElement("div");
-divMapa.setAttribute("id", "mapa");
+    const divTodo = document.createElement("div");
+    divTodo.setAttribute("id","divTodo");
+
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    const divMapa = crearDivMapa();
+    const divDatos = crearDivDatos();
+    
+    const divBusqueda = crearDivBusqueda();
+    divMapa.appendChild(divBusqueda);
+
+    row.appendChild(divDatos);
+    row.appendChild(divMapa);
+
+    divTodo.appendChild(row);
+    main.appendChild(divTodo);
+    body.appendChild(main);
+
+}
+
+//crear div para el mapa
+function crearDivMapa() {
+    const divMapa = document.createElement("div");
+    divMapa.setAttribute("id","mapa");
+    divMapa.classList.add("col-md-6");
+    return divMapa;
+}
+
+//crear div para los datos
+function crearDivDatos() {
+    const divDatos = document.createElement("div");
+    divDatos.setAttribute("id","datos");
+    divDatos.classList.add("col-md-6");
+    return divDatos;
+}
+
+//crear el div de busqueda
+function crearDivBusqueda() {
+    const divBusqueda = document.createElement("div");
+    divBusqueda.setAttribute("id","busqueda");
+    return divBusqueda;
+}
+
+//cargar los archivos para el MapBox
+
+function cargarMapbox(){
+    const linkcss = document.createElement("link");
+    linkcss.setAttribute("href","https://api.mapbox.com/mapbox-gl-js/v2.9.0/mapbox-gl.css");
+    linkcss.setAttribute("rel","stylesheet");
+    head.appendChild(linkcss);
+
+    const scriptMapa = document.createElement("script");
+    scriptMapa.setAttribute("src","https://api.mapbox.com/mapbox-gl-js/v2.9.0/mapbox-gl.js");
+    scriptMapa.onload = function() {
+        cargarBuscador();
+        configurarMapa();
+    };
+    body.appendChild(scriptMapa);
 
 
-// Añadir divDatos
-const divDatos = document.createElement("div");
-divDatos.setAttribute("id", "datos");
-
-divTodo.appendChild(divDatos);
-divTodo.appendChild(divMapa);
-
-main.appendChild(divTodo);
-body.appendChild(main);
-
-// Crear los links 
-const linkcss = document.createElement("link");
-linkcss.setAttribute("href", "https://api.mapbox.com/mapbox-gl-js/v2.9.0/mapbox-gl.css");
-linkcss.setAttribute("rel", "stylesheet");
-head.appendChild(linkcss);  // Usar appendChild en el head
-
-// Crear el script y agregarlo al body
-const scriptMapa = document.createElement("script");
-scriptMapa.setAttribute("src", "https://api.mapbox.com/mapbox-gl-js/v2.9.0/mapbox-gl.js");
-scriptMapa.onload = function() {
-    // Llamar la función para cargar el mapa después de que Mapbox se haya cargado
-    cargarMapa();
-};
-body.appendChild(scriptMapa); // Agregar el script al body
-
-// Crear la función cargarMapa
-function cargarMapa() {
-    mapboxgl.accessToken = 'pk.eyJ1IjoibWFpazEyNCIsImEiOiJjbTcwczVmeGowNGpsMmpzbmlybmpuajByIn0.s2p0IIfjzS1fGa52vRd4iQ';
-    const map = new mapboxgl.Map({
-        container: 'mapa', // ID del div donde se mostrará el mapa
-        style: 'mapbox://styles/mapbox/streets-v11', // Estilo del mapa
-        center: [-74.5, 40], // Coordenadas iniciales [longitud, latitud]
-        zoom: 9 // Nivel de zoom inicial
-    });
-
-    map.on('load', () => {
-        document.getElementById('mapa').style.height = '500px';
-        document.getElementById('mapa').style.width = '100%';
-    });
 }
 
 
-//hay que añadir las rutas y objetivo que salga el nombre del usuario 
+//funcion para cargar el buscador
+function cargarBuscador() {
+    const searchmap = document.createElement("script");
+    searchmap.setAttribute("src", "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.0/mapbox-gl-geocoder.min.js");
+    searchmap.onload = function () {
+        configurarBuscador();
+    };
+    body.appendChild(searchmap);
+}
+
+//Configurar el mapa
+
+function configurarMapa() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWFpazEyNCIsImEiOiJjbTcwczVmeGowNGpsMmpzbmlybmpuajByIn0.s2p0IIfjzS1fGa52vRd4iQ';
+    map = new mapboxgl.Map({
+        container: 'mapa',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-74.5, 40],
+        zoom: 9
+    });
+
+    const navControl = new mapboxgl.NavigationControl();
+    map.addControl(navControl, 'bottom-right');
+
+    map.on('load', () => {
+        document.getElementById('mapa').style.height = '90vh';
+        document.getElementById('mapa').style.width = '50%';
+        map.resize();
+    });
+
+    window.addEventListener('resize', () => map.resize());
+}
+
+//Configurar el buscador
+
+function configurarBuscador() {
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    });
+
+    document.getElementById('busqueda').appendChild(geocoder.onAdd(map));
+
+    geocoder.on('result', (e) => {
+        const { center } = e.result.geometry;
+        map.flyTo({
+            center: center,
+            zoom: 12
+        });
+    });
+}
+
+//obtener y mostrar eventos de la api de tickmaster
+
+function obtenerDatosTickmaster() {
+    const apikeyTick = "vykajZlL73mCQ8NHCPW6KbHeCanHcFf5";
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apikeyTick}&size=200`)
+        .then(response => response.json())
+        .then(data => mostrarEventos(data._embedded.events))
+        .catch(error => console.log('Error al obtener los eventos:', error));
+}
+
+
+//mostrar datos de tickmaster
+
+function mostrarEventos(eventos) {
+    const listaEventos = document.createElement("ul");
+
+    eventos.forEach(evento => {
+        const li = document.createElement("li");
+        const tipo_event = obtenerTipoEvento(evento);
+
+        //aqui obtengo la imgen y si no exite usamos una predeterminada
+        const imagentick = evento.images && evento.images[0] ? evento.images[0].url : 'default-image.jpg';
+
+        //li.textContent = `${evento.name} - ${evento.dates.start.localDate} - ${evento.dates.start.localTime} - ${evento._embedded.venues[0].name} - ${evento._embedded.venues[0].city.name} - ${evento._embedded.venues[0].country.name} - ${tipo_event}`;
+
+        li.innerHTML = `
+        <div class= "evento-info">
+        <img src="${imagentick}" alt="${evento.name} class "evento-imagen">
+        <p><strong>${evento.name}</strong></p>
+        <p>${evento.dates.start.localDate} - ${evento.dates.start.localTime}</p>
+        <p>${evento._embedded.venues[0].name} - ${evento._embedded.venues[0].city.name} - ${evento._embedded.venues[0].country.name}</p> 
+        <a href="${evento.url}" target="_blank" class="comprar-entradas">Comprar Entradas </a>
+        </div>
+        `
+        
+
+        // const enlace = document.createElement("a");
+        // enlace.href = evento.url;
+        // enlace.textContent = "Comprar Entradas";
+        // enlace.target = "_blank";
+
+        // li.appendChild(enlace);
+        listaEventos.appendChild(li);
+    });
+
+    document.getElementById("datos").appendChild(listaEventos);
+
+}
+
+//funcion para obtener los tipos de eventos de tickmaster
+
+function obtenerTipoEvento(evento) {
+    return evento.classifications && evento.classifications[0] ? evento.classifications[0].segment.name : "Desconocido";
+}
+
+//funcion para crear el estilo de la lista del contenido de datos 
+
+function estiloDato() {
+    
+}
+
+//llamada a window onload que carga las funciones 
+window.onload = function() {
+    initMapa();
+    obtenerDatosTickmaster();
+}
--- public/views/login/login.js
+++ public/views/login/login.js
@@ -1,90 +1,119 @@
-window.onload = function(){
-
-    //Div form 
+// funcion que crea el div del formulario y luego devuelve el contenedor principal
+function createDivForm() {
     const divForm = document.createElement("div");
-    divForm.setAttribute("id","divForm");    
-    divForm.classList.add("d-flex","justify-content-center","align-items-center","min-vh-100","position-sticky");
-    divForm.style.backgroundColor = "aqua";
+    divForm.setAttribute("id", "divForm");
+    divForm.classList.add("d-flex", "justify-content-center", "align-items-center", "min-vh-100", "position-sticky");
+    divForm.style.backgroundColor = "aqua";  
+    return divForm;
+}
 
-    //Creo el formulario
+//funcion que crea y devuelve el formulario 
+function createForm() {
     const form = document.createElement("form");
-    form.setAttribute("id","loginForm");
-    form.classList.add("container","p-4","border","rounded","d-flex","flex-column","align-items-center");
-    form.style.maxWidth = "400px";
-    form.style.minHeight = "auto";
+    form.setAttribute("id", "loginForm");
+    form.classList.add("container", "p-4", "border", "rounded", "d-flex", "flex-column", "align-items-center");
+    form.style.maxWidth = "400px";  
+    form.style.minHeight = "auto";  
+    return form;
+}
 
-
-    //Titulo Login
+//funcion que crea y devuelve el titulo del formulario
+function createTitle(text) {
     const title = document.createElement("h2");
-    title.textContent = "Bienvenido";
-    title.classList.add("text-center","mb-4","text-primary");
+    title.textContent = text;
+    title.classList.add("text-center", "mb-4", "text-primary");  
+    return title;
+}
 
-    //Creo el contenedor para cada campo del formulario
+//funcion que crear y devuelve el contendor de los campos para el formulario 
+function createFormContainer() {
+    // Crear el contenedor para los campos del formulario
     const formContainer = document.createElement("div");
-    formContainer.setAttribute("id","div1");
 
-    //creo la etiqueta y el campo para el usuario 
-    const labelUsuario = document.createElement("label");
-    labelUsuario.setAttribute("for","usuario");
-    labelUsuario.classList.add("form-label");
-    labelUsuario.textContent = "Usuario";
+    // Establecer el id del contenedor
+    formContainer.setAttribute("id", "div1");
 
+    return formContainer;
+}
 
-    const inputUsuario = document.createElement("input");
-    inputUsuario.setAttribute("type","text");
-    inputUsuario.setAttribute("placeholder","Usuario");
-    inputUsuario.setAttribute("id","Usuario");
-    inputUsuario.classList.add("form-control","mb-3","form-control-sm","w-100","rounded-pill");
-    
-    
-    //Agrego los elemento al contendor Form
+//funcion que crea los los campoy y inputs del formulario 
+function createLabel(text) {
+    const label = document.createElement("label");
+    label.classList.add("form-label");  
+    label.textContent = text;
+    return label;
+}
+
+function createInput(type, placeholder, id) {
+    const input = document.createElement("input");
+    input.setAttribute("type", type);
+    input.setAttribute("placeholder", placeholder);
+    input.setAttribute("id", id);
+    input.classList.add("form-control", "mb-3", "form-control-sm", "w-100", "rounded-pill");  
+    return input;
+}
+
+//funcion que crea y devuelve un boton 
+function createButton(type, text, id) {
+    const button = document.createElement("button");
+    button.setAttribute("type", type);
+    button.setAttribute("id", id);
+    button.classList.add("btn", "btn-primary", "w-75", "rounded-pill");  
+    button.textContent = text;
+    return button;
+}
+
+//funcion que crea los enlaces en un href y tambien un texto 
+function createLink(href, text) {
+    const link = document.createElement("a");
+    link.setAttribute("href", href);
+    link.textContent = text;
+    link.classList.add("d-block", "text-decoration-none");  
+    return link;
+}
+
+// Llamada a window.onload para ejecutar el codigo
+window.onload = function () {
+    // Crear el contenedor principal
+    const divForm = createDivForm();
+
+    // Crear el formulario
+    const form = createForm();
+
+    // Crear título
+    const title = createTitle("Bienvenido");
+
+    // Crear contenedor para los campos del formulario y agregarlo al formulario
+    const formContainer = createFormContainer();  
+    form.appendChild(formContainer);  
+
+    // Crear campos de usuario y contraseña
+    const labelUsuario = createLabel("Usuario");
+    const inputUsuario = createInput("text", "Usuario", "usuario");
+
+    const labelPassword = createLabel("Contraseña");
+    const inputPassword = createInput("password", "Contraseña", "password");
+
+    // Agregar campos al contenedor del formulario
     formContainer.appendChild(labelUsuario);
     formContainer.appendChild(inputUsuario);
-
-    //creo la etiqueta y el campo para la password
-    const labelPassword = document.createElement("label");
-    labelPassword.setAttribute("for","password");
-    labelPassword.classList.add("form-label");
-    labelPassword.textContent = "Contraseña";
-
-
-    const inputPassword = document.createElement("input");
-    inputPassword.setAttribute("type","password");
-    inputPassword.setAttribute("placeholder","Contraseña");
-    inputPassword.setAttribute("id","password");
-    inputPassword.classList.add("form-control","mb-3","form-control-sm","w-100","rounded-pill");
-
-    //Agrego los elemento al contendor Form
     formContainer.appendChild(labelPassword);
     formContainer.appendChild(inputPassword);
 
-    //Creo el boton de login 
-    const btnLogin = document.createElement("button");
-    btnLogin.setAttribute("type","submit");
-    btnLogin.setAttribute("id","btnSubmit");
-    btnLogin.classList.add("btn","btn-primary","w-75","rounded-pill");
-    btnLogin.textContent = "Inciar Sesion";
+    // Crear botón de login
+    const btnLogin = createButton("submit", "Iniciar Sesión", "btnSubmit");
 
-    //Agrego el formulario al contenedor principal 
+    // Crear link
+    const link = createLink("/registro/registro.html", "¿No tienes cuenta? Regístrate");
+
+    // Agregar elementos al formulario
     form.appendChild(title);
-    form.appendChild(formContainer);
     form.appendChild(btnLogin);
-
-    //agrego el link 
-    const link = document.createElement("a");
-    link.setAttribute("href","/registro/registro.html");
-    link.textContent = "¿No Tienes Cuenta? Regístrate";
-    link.classList.add("d-block","text-decoration-none");
-
-    //agrego el link al formulario
     form.appendChild(link);
 
-    //Agrego el formulario al div
+    // Agregar el formulario al contenedor principal
     divForm.appendChild(form);
 
-    //Agrego el formulario al main 
+    // Agregar el formulario al main
     document.querySelector("main").appendChild(divForm);
-
-
-    
-}
+};
