ec912fd725ba39a4b3146e476773ded8b48f5ddb
Michael Giraldo <michaelgiraldo320@gmail.com>
2025-03-03 21:53:44 +0100

    cambios popup mapa

--- public/views/dashboard/mapa.js
+++ public/views/dashboard/mapa.js
@@ -251,9 +251,52 @@ function elimMarcadores() {
     });
     marcadores = []; //vacio el array de marcadores
 }
-function popUp(error) {
-  
+function mostrarPopUp(mensaje) {
+
+  const popup = document.createElement("div");
+  popup.classList.add("modal");
+
+  const exitePopUp = document.querySelector(".modal");
+  if (exitePopUp) {
+    //si exite el popup, no duplicara el popup
+    return;
+  }
+
+  const poupContent = document.createElement("div");
+  poupContent.classList.add("modal-content");
+
+  const closeBtn = document.createElement("button");
+  closeBtn.classList.add("close");
+  closeBtn.textContent = "X";
+
+  const message = document.createElement("p");
+  message.textContent = mensaje;
+
+  //agregar los elementos
+
+  poupContent.appendChild(closeBtn);
+  poupContent.appendChild(message);
+  popup.appendChild(poupContent);
+
+  document.body.appendChild(popup);
+
+  popup.style.display = "block";
+
+
+  closeBtn.addEventListener("click", function (event) {
+    popup.style.display = "none";
+    document.body.removeChild(popup);
+  });
+
+  setTimeout(function(){
+    if (popup.style.display !== "none") {
+      popup.style.display = "none";
+      document.body.removeChild(popup);
+    }
+  }, 3000);
+
 }
+
 function obtenerDatosTickmaster(tipo,ciudad) {
     const apikeyTick = "vykajZlL73mCQ8NHCPW6KbHeCanHcFf5";
     let url=`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apikeyTick}&radius=10&size=50`
@@ -269,8 +312,8 @@ function obtenerDatosTickmaster(tipo,ciudad) {
             if (data._embedded && data._embedded.events) {//si la respuesta tiene el campo _embedded.events, mostramos eventos
                 mostrarEventos(data._embedded.events);
             } else {
-                console.log('No se encontraron eventos.');//hacer un pop-up
-                console.log(data);//hacer un pop-up
+              mostrarPopUp('No se encontraron eventos.');//hacer un pop-up
+              console.log(data);//hacer un pop-up
             }
         })
         .catch(error => {
