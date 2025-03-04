83d46a2d4efeb20046dc260cf7a177f3a583f8f3
Luis0sorio <luiseosorio96@gmail.com>
2025-03-04 15:25:28 +0100

    logout, favoritos 2.0

--- models/Token.js
+++ models/Token.js
@@ -7,8 +7,5 @@ const tokenSchema = new Schema({
   expiracion: { type:Date, required: true },
 });
 
-// Crear un índice para eliminar tokens expirados automáticamente
-//tokenSchema.index({expiracion: 1}, {expireAfterSeconds: 0});
-
 const Token = mongoose.model("Token", tokenSchema);
 module.exports = Token;
